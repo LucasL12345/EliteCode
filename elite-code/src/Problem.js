1f1445e76565260909c1f384a196bc1a5eddc425
Lucas <lucaslemoine49@gmail.com>
2023-06-10 00:55:46 +0100

    refactored code to ensure appropriate padding

--- elite-code/src/Problem.js
+++ elite-code/src/Problem.js
@@ -7,68 +7,66 @@ import Split from 'react-split';
 import 'ace-builds/src-noconflict/mode-python';
 import 'ace-builds/src-noconflict/theme-monokai';
 
-
 function Problem() {
-  const { id } = useParams();
-  const problem = problemData.find(problem => problem.id === parseInt(id));
+    const { id } = useParams();
+    const problem = problemData.find(problem => problem.id === parseInt(id));
 
-  const [code, setCode] = useState('');
-  const [output, setOutput] = useState('');
+    const [code, setCode] = useState('');
+    const [output, setOutput] = useState('');
 
-  const handleCodeChange = (newCode) => {
-    setCode(newCode);
-  };
+    const handleCodeChange = (newCode) => {
+        setCode(newCode);
+    };
 
-  const handleRunClick = async () => {
-    const response = await fetch('http://localhost:4000/run', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ code }),
-    });
+    const handleRunClick = async () => {
+        const response = await fetch('http://localhost:4000/run', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ code }),
+        });
 
-    const output = await response.text();
-    setOutput(output);
-  };
+        const output = await response.text();
+        setOutput(output);
+    };
 
-  return (
-    <Split 
-        sizes={[50, 50]} 
-        minSize={100} 
-        expandToMin={true} 
-        gutterSize={10} 
-        className="split"
-        gutter={(index, direction) => {
-            const gutter = document.createElement('div');
-            gutter.className = `gutter gutter-${direction}`;
-            return gutter;
-        }}
-    >
-      <div>
-        <h2>{problem.title}</h2>
-        <h3>Description</h3>
-        <p>{problem.description}</p>
-      </div>
-      <div>
-  <h3>Editor</h3>
-  <AceEditor
-      mode="python"
-      theme="monokai"
-      onChange={handleCodeChange}
-      editorProps={{ $blockScrolling: true }}
-      wrapEnabled={true}
-      style={{ width: '100%' }}
-  />
-  <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-    <h3>Output</h3>
-    <button onClick={handleRunClick}>Run</button>
-  </div>
-  <textarea readOnly value={output} />
-</div>
-
-    </Split>
-  );
+    return (
+        <Split
+            sizes={[50, 50]}
+            minSize={100}
+            expandToMin={true}
+            gutterSize={10}
+            className="split"
+            gutter={(index, direction) => {
+                const gutter = document.createElement('div');
+                gutter.className = `gutter gutter-${direction}`;
+                return gutter;
+            }}
+        >
+            <div className="problem-section">
+                <h2>{problem.title}</h2>
+                <h3>Description</h3>
+                <p>{problem.description}</p>
+            </div>
+            <div className="editor-section">
+                <h3>Editor</h3>
+                <AceEditor
+                    mode="python"
+                    theme="monokai"
+                    onChange={handleCodeChange}
+                    editorProps={{ $blockScrolling: true }}
+                    wrapEnabled={true}
+                    style={{ width: '100%' }}
+                />
+                <div className="output-section">
+                    <h3>Output</h3>
+                    <button onClick={handleRunClick}>Run</button>
+                </div>
+                <textarea readOnly value={output} />
+            </div>
+        </Split>
+    );
 }
 
 export default Problem;
