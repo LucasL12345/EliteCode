02984426632c1f747c3b65de3b6c6f50b7eecaab
Lucas <lucaslemoine49@gmail.com>
2023-06-11 11:53:30 +0100

    styled table in Problems page

--- elite-code/src/Problems.js
+++ elite-code/src/Problems.js
@@ -1,5 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 import problemData from './problems.json';
 
 function Problems() {
@@ -10,25 +18,36 @@ function Problems() {
   }, []);
 
   return (
-    <div>
-      <h2>Problems</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Title</th>
-          </tr>
-        </thead>
-        <tbody>
-          {problems.map((problem) => (
-            <tr key={problem.id}>
-              <td>
-                <Link to={{ pathname: `/problems/${problem.id}` }}>{problem.title}</Link>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <Box sx={{ width: '85%', margin: '0 auto', mt: 2, overflowX: 'auto' }}>
+      <Typography variant="h4" align="center" gutterBottom>Problems</Typography>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="problems table">
+          <TableHead>
+            <TableRow sx={{ backgroundColor: '#fff' }}>
+              <TableCell sx={{width: '70%'}}>Problem Title</TableCell>
+              <TableCell sx={{width: '15%'}} align="center">Difficulty</TableCell>
+              <TableCell sx={{width: '15%'}} align="center">Completed</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {problems.map((problem) => (
+              <TableRow
+                key={problem.id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                onClick={() => {window.location.href=`/problems/${problem.id}`}}
+                sx={{ '&:hover': { backgroundColor: 'rgba(0,0,0,0.04)' }, cursor: 'pointer'}}
+              >
+                <TableCell component="th" scope="row" style={{ backgroundColor: '#fff' }}>
+                  {problem.title}
+                </TableCell>
+                <TableCell align="center">Placeholder</TableCell>
+                <TableCell align="center">Placeholder</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
   );
 }
 
