4bdaaab5f12364d81c49fbd242ced525d113c357
Lucas <lucaslemoine49@gmail.com>
2023-07-14 21:07:51 +0100

    refactored App.js to allow users to be redirected on logout

--- elite-code/src/App.js
+++ elite-code/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link, useNavigate } from 'react-router-dom';
 import Home from './Home';
 import Problems from './Problems';
 import Problem from './Problem';
@@ -9,8 +9,46 @@ import Submitted from './Submitted'
 import './App.css';
 import axios from 'axios';
 
+function Navigation({ username, setUsername }) {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    setUsername('');
+    delete axios.defaults.headers.common['Authorization'];
+    localStorage.removeItem('username');
+    localStorage.removeItem('token');
+    navigate('/problems');
+  };
+
+  return (
+    <ul>
+      <li>
+        <Link to="/" className={window.location.pathname === '/' ? 'active' : ''}>Home</Link>
+      </li>
+      <li>
+        <Link to="/problems" className={window.location.pathname.startsWith('/problems') ? 'active' : ''}>Problems</Link>
+      </li>
+      <li className="right">
+        {username ? (
+          <>
+            <span className="nav-username">{username}</span>
+            <button onClick={handleLogout} className="nav-logout">Logout</button>
+          </>
+        ) : (
+          <>
+            <Link to="/login">Login</Link>
+            <span> | </span>
+            <Link to="/register">Register</Link>
+          </>
+        )}
+      </li>
+    </ul>
+  );
+}
+
 function App() {
   const [username, setUsername] = useState('');
+
   useEffect(() => {
     const storedUsername = localStorage.getItem('username');
     if (storedUsername) {
@@ -23,39 +61,11 @@ function App() {
     localStorage.setItem('username', username);
   };
 
-  const handleLogout = () => {
-    setUsername('');
-    delete axios.defaults.headers.common['Authorization'];
-    localStorage.removeItem('username');
-    localStorage.removeItem('token');
-  };
-
   return (
     <Router>
       <div>
         <nav>
-          <ul>
-            <li>
-              <Link to="/" className={window.location.pathname === '/' ? 'active' : ''}>Home</Link>
-            </li>
-            <li>
-              <Link to="/problems" className={window.location.pathname.startsWith('/problems') ? 'active' : ''}>Problems</Link>
-            </li>
-            <li className="right">
-              {username ? (
-                <>
-                  <span className="nav-username">{username}</span>
-                  <button onClick={handleLogout} className="nav-logout">Logout</button>
-                </>
-              ) : (
-                <>
-                  <Link to="/login">Login</Link>
-                  <span> | </span>
-                  <Link to="/register">Register</Link>
-                </>
-              )}
-            </li>
-          </ul>
+          <Navigation username={username} setUsername={setUsername} />
         </nav>
 
         <Routes>
@@ -72,3 +82,4 @@ function App() {
 }
 
 export default App;
+
