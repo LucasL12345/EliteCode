8034c5381d15955e3c7894db3ce8e829c7799b53
Lucas <lucaslemoine49@gmail.com>
2023-07-15 08:37:07 +0100

    created and styled home page

--- elite-code/src/Home.js
+++ elite-code/src/Home.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './App.css';
+import home_img from './images/home_img.jpeg'
 
 function Home() {
   return (
-    <div>
-      <h2>Home</h2>
+    <div className="home-container">
+      <div className="home-text">
+        <h1>Welcome to Elitecode!</h1>
+        <p>Get started with improving your coding skills today. Elitecode offers a wide variety of coding problems for you to solve. Are you up for the challenge?</p>
+        <button className="home-btn">Get Started</button>
+      </div>
+      <div className="home-image">
+        <img src={home_img} alt="img"/>
+      </div>
     </div>
   );
 }
